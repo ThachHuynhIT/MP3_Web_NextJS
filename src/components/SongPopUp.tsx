f57d5f126afb5e4b8941250a7bb9271335e9da41
ThachHuynhIT <thachhuynhit.ut@gmail.com>
2023-03-08 11:22:21 +0700

    song list update

--- src/components/SongPopUp.tsx
+++ src/components/SongPopUp.tsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { BsThreeDots } from "react-icons/bs";
+import useOnClickOutside from "@/hooks/useOnClinkOutSide";
 
 interface props {
-  index: any;
+  index: number;
 }
 
 const SongPopUp = ({ index }: props) => {
+  const buttonRef = useRef<any>();
+  const [isOpen, setOpen] = useState<boolean>(false);
+  useOnClickOutside(buttonRef, () => {
+    let a = { index };
+    let drop = document.getElementById(a.index.toString());
+    drop?.classList.add("hidden");
+    setOpen(false);
+  });
+
+  useEffect(() => {
+    return () => {};
+  }, [isOpen]);
+
   const handleClick = () => {
     let a = { index };
-    let drop = document.getElementById(a.toString());
-    console.log(a);
+    let drop = document.getElementById(a.index.toString());
+    if (!isOpen) {
+      drop?.classList.remove("hidden");
+      setOpen(true);
+    } else {
+      drop?.classList.add("hidden");
+      setOpen(false);
+    }
   };
 
   return (
-    <div>
+    <div ref={buttonRef}>
       <div className="flex justify-center">
         <div>
           <div className="relative">
@@ -22,19 +42,18 @@ const SongPopUp = ({ index }: props) => {
               className="flex items-center whitespace-nowrap rounded bg-primary leading-normal text-white motion-reduce:transition-none"
               type="button"
             >
-              {index}
               <BsThreeDots></BsThreeDots>
             </button>
             <ul
-              id={index}
-              className="absolute right-5 top-[-10px] z-[1000] float-left m-0 min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700"
+              id={index.toString()}
+              className="absolute hidden right-5 top-[-10px] z-[1000] float-left m-0 min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700"
             >
               <li>
                 <a
                   className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
                   href="#"
                 >
-                  Action
+                  Add to playlist
                 </a>
               </li>
               <li>
@@ -42,7 +61,7 @@ const SongPopUp = ({ index }: props) => {
                   className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
                   href="#"
                 >
-                  Another action
+                  Go to album
                 </a>
               </li>
               <li>
@@ -50,7 +69,7 @@ const SongPopUp = ({ index }: props) => {
                   className="block w-full whitespace-nowrap bg-transparent py-2 px-4 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
                   href="#"
                 >
-                  Something else here
+                  Add to queue
                 </a>
               </li>
             </ul>
--- src/components/SongsList.tsx
+++ src/components/SongsList.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import songs from "../../data/songs";
-import { BsThreeDots } from "react-icons/bs";
 import SongPopUp from "./SongPopUp";
+import Image from "next/image";
 
 export const SongsList = () => {
-  console.log(songs);
+  // console.log(songs);
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg mx-4">
+    <div className="relative overflow-x-auto shadow-md scrollbar-hidden sm:rounded-lg mx-4 bg-gradient-to-tr from-violet-500 to-black bggrad ">
+      <div className="m-4 ">
+        <Image
+          priority
+          className="rounded-t-lg h-44 "
+          src="/image/2.jpg"
+          alt=""
+          width={200}
+          height={200}
+        />
+      </div>
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <thead className="text-xs text-gray-800 uppercase dark:text-gray-400">
           <tr>
             <th scope="col" className="px-6 py-3">
               #
@@ -19,19 +29,14 @@ export const SongsList = () => {
             <th scope="col" className="px-6 py-3">
               Author
             </th>
-            <th scope="col" className="px-6 py-3">
-              Url
-            </th>
+            <th scope="col" className="px-6 py-3"></th>
           </tr>
         </thead>
         <tbody>
           {songs.map((songs, index) => {
             return (
               <>
-                <tr
-                  key={index}
-                  className="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
-                >
+                <tr key={index} className="border-b dark:border-gray-700">
                   <td className="px-6 py-4">{index + 1}</td>
                   <th
                     scope="row"
@@ -41,7 +46,6 @@ export const SongsList = () => {
                   </th>
                   <td className="px-6 py-4">{songs.author}</td>
                   <td className="px-6 py-4 absolute ">
-                    {/* <BsThreeDots /> */}
                     <SongPopUp index={index} />
                   </td>
                 </tr>
